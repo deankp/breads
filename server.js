642755980f19fb6f9f9b43c2b2c9d07188a497dc
deankp <dperez0689@gmail.com>
2022-06-16 19:36:41 -0500

    incorporated mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -34,3 +35,8 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
 })
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
